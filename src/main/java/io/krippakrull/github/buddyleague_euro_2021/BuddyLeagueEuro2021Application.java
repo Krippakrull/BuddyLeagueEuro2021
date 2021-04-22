02713bfe8c0954ba56fab6728a1c1b61a05c65ef
Krippakrull <56680747+Krippakrull@users.noreply.github.com>
2021-04-22 14:40:42 +0200

    Initial commit

---
+++ src/main/java/io/krippakrull/github/buddyleague_euro_2021/BuddyLeagueEuro2021Application.java
@@ -0,0 +1,13 @@
+package io.krippakrull.github.buddyleague_euro_2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddyLeagueEuro2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuddyLeagueEuro2021Application.class, args);
+    }
+
+}
---
+++ src/test/java/io/krippakrull/github/buddyleague_euro_2021/BuddyLeagueEuro2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.krippakrull.github.buddyleague_euro_2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuddyLeagueEuro2021ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
